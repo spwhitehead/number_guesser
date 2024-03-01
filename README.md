e98600fa04b8d132c4af2bf361d470ab1797bbe1
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-03-01 08:31:22 -0700

    Initial Commit
    Instantiates number_guesser.py and adds the file definition to the README.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # number_guesser
-A simple number guessing game with a binary search tree
+Make a number guesser where the program tries to guess your number! You will pick a number and feed it to the program,
+then the program will try to guess it. If it's wrong, you'll tell it whether it was too high or too low, then it will guess again.
+Use a binary search algorithm for the guessing.
---
+++ number_guesser.py
@@ -0,0 +1,21 @@
+
+"""
+Make a number guesser where the program tries to guess your number! You'll pick a number (don't give it to the program), 
+then the program tries to guess it. If it's wrong, you'll tell it whether it was too high or too low, then it'll guess again. 
+Use a binary search algorithm for the guessing.
+"""
+def guess_a_number():
+    """ Utilizes a binary search tree to search for the number """
+    pass
+
+
+def is_it_right():
+    """ Asks the user if the guessed number is correct and takes input from the user """
+
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
